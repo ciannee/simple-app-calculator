fa520b75170fb985e1f4aa7fb705cae7769155b3
Patricia Pineda <pepineda28@gmail.com>
2023-05-07 01:20:24 +0800

    pseudocode

--- simple-app-calculator.py
+++ simple-app-calculator.py
@@ -1,3 +1,25 @@
 # Name: Pineda, Patricia Anne E.
 # Section: BSCPE 1-5
 # Assigment 5: Simple App Calculator
+
+# looping: 
+
+    # ask user for input: math operations
+
+    # ask user for input: float
+
+    # addition
+
+    # subtraction
+
+    # multiplication
+
+    # division
+
+# looping: ask user to try again
+
+    # if yes
+
+    # if no
+
+    # else invalid input

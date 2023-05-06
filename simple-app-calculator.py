2e8f4aaebbd838c44b6bfb8ae9e86ac0d78ecec5
Patricia Pineda <pepineda28@gmail.com>
2023-05-07 01:57:20 +0800

    ask user for input

--- simple-app-calculator.py
+++ simple-app-calculator.py
@@ -7,11 +7,17 @@ simple_calculator = ""
 while simple_calculator != "NO":
 
     # ask user for input: math operations
-    operation = input("\n\033[01mHi! Pleases choose among the four math operations:\n\nAddition\nSubtraction\nMultiplication\nDivision")
+    operation = input("\n\033[01mHi! Pleases choose among the four math operations:\n\nAddition\nSubtraction\nMultiplication\nDivision\n\nEnter your answer: ")
+   
     # ask user for input: float
+    print ("\n\nGreat! Please enter two numbers of your choice!")
+    num1 = input ("Enter your first number: ")
+    print("Your first value is: ", num1)
+    num2 = input ("\nEnter your second number: ")
+    print("Your second value is: ", num2)
 
     # addition
-
+    
     # subtraction
 
     # multiplication
